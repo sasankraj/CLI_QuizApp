2128a667af19a40c2e842f51a9f231b354611654
sasankraj <52368934+sasankraj@users.noreply.github.com>
2021-04-25 23:11:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,137 @@
+const chalk = require('chalk');
+console.log(chalk.magenta("--------------------------------"));
+console.log(chalk.blue.bold("|Welcome to the Easy Quiz on JS|"));
+console.log(chalk.magenta("--------------------------------"));
+const r = require('readline-sync');
+const name = r.question("\nPlease enter your name : ");
+instructions(name);
+const questions = [
+  {
+    question : 
+              "Which of the following is true about variable naming conventions in JavaScript ?",
+    options : 
+              [
+                "JavaScript variable names must begin with a letter or the underscore character",
+                "JavaScript variable names are case sensitive",
+                "Both of these",
+                "None of these"
+              ],
+    answer : 3
+  },
+
+  {
+    question : 
+              "Which of the following type of variable takes precedence over other if their names are same?",
+              
+    options :
+              [ "Local variable", "Global variable", "Reference variable", "Static variable" ],
+
+    answer : 1
+  },
+
+  {
+    question : 
+              "Which of the following function of String object returns the character at the specified index?",
+
+    options : 
+              [ "charCodeAt()", "charAt()", "indexOf()", "console()" ],
+
+    answer : 2
+  }, 
+
+  {
+    question : 
+              "Which of the following function of Array object calls a function for each element in the array?",
+
+    options : 
+              [ "concat()", "every()", "forEach()", "filter()" ],
+
+    answer : 3
+  },
+
+  {
+    question : 
+              "Callbacks are _________ that are passed as arguments to another function.",
+
+    options : 
+              [ "Strings", "Variables", "Arrays", "Functions" ],
+
+    answer : 4
+  }
+
+];
+
+const count = questions.length; // total number of questions
+let score = 0; // for showing the user score
+main();
+
+
+// user defined functions start from here
+function main() {
+  display(count);
+  conclusion(score, count, name);
+  const again = r.question('wanna try again? (y/any other key to quit)\t');
+  if(again === 'y') {
+    main();
+  }
+}
+
+function display() {
+  console.log('--------------------------------------------------------------------------------');
+  for(let i = 0; i < count; i++) {
+    console.log();
+    console.log("Q" + (i+1).toString() + ". " + questions[i]['question'] + '\n');
+    let lp = questions[i]['options'].length;
+    for(const opts of questions[i]['options']) {
+      console.log('(' + (questions[i]['options'].length - (lp--) + 1).toString() + ') ' + opts + '\n');
+    }
+    const choice = r.question("enter your choice : ");
+    validate(i , choice);
+    console.log();
+    console.log('--------------------------------------------------------------------------------');
+  }
+   
+}
+
+function validate(i, ch) 
+{
+  if(![1,2,3,4].includes(Number(ch))) {
+    console.log("You entered an invalid choice");
+  }
+
+  else if(questions[i]['answer'].toString() === ch) {
+    console.log("Well done!! you have answered correctly.");
+    ++score;
+  }
+
+  else {
+    console.log("Oopsie!! That is incorrect");
+    console.log("The correct answer is : " + questions[i]['answer'].toString());
+  }
+
+  console.log();
+}
+
+function instructions(name) {
+  console.log("\nGreetings " + name.toUpperCase()+"!");
+  console.log(chalk.redBright.underline.bold("PLEASE FOLLOW THE INSTRUCTIONS \n"));
+  console.log("Each question will have four choices of which only one is correct.");
+  console.log("You will have only one chance to guess the correct answer.\n");
+  console.log("All the best!\n".toUpperCase());
+}
+
+
+function conclusion(score, count, name) {
+  if (score === 0) {
+  console.log("Oops!! you have scored " + score + " out of " + count);
+  console.log("Practice to get better.");
+  }
+  else if(score<count/2) {
+  console.log("Well played!! you have scored " + score + " out of " + count);
+  console.log("Keep trying, you can improve");
+  }
+  else {
+  console.log("Congratulations you have scored " + score + " out of " + count);
+  }
+  console.log("Thanks for playing " + name + ". Good Bye!!\n");
+}
