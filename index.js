4728f49b513392d2ea3e592f499aef1753666ea6
sasankraj <52368934+sasankraj@users.noreply.github.com>
2021-04-26 00:10:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,77 @@
+// user defined functions start from here
+
+const disp = () => {
+  console.log('--------------------------------------------------------------------------------');
+  for(let i = 0; i < count; i++) {
+    console.log();
+    console.log("Q" + (i+1).toString() + ". " + questions[i]['question'] + '\n');
+    let optNum = questions[i]['options'].length;
+    for(const opts of questions[i]['options']) {
+      console.log('(' + (questions[i]['options'].length - (optNum--) + 1).toString() + ') ' + opts + '\n');
+    }
+    const choice = r.question("enter your choice : ");
+    validate(questions[i], choice);
+    console.log();
+    console.log('--------------------------------------------------------------------------------');
+  }
+   
+}
+
+function validate(q, ch) 
+{
+  if(![1,2,3,4].includes(Number(ch))) {
+    console.log("You entered an invalid choice");
+  }
+
+  else if(q['answer'].toString() === ch) {
+    console.log("Well done!! you have answered correctly.");
+    ++score;
+  }
+
+  else {
+    console.log("Oopsie!! That is incorrect");
+    console.log("The correct answer is : " + q['answer'].toString());
+  }
+
+  console.log();
+}
+
+const instructions = (name) => {
+  console.log("\nGreetings " + name.toUpperCase()+"!");
+  console.log(chalk.redBright.underline.bold("PLEASE FOLLOW THE INSTRUCTIONS \n"));
+  console.log("Each question will have four choices of which only one is correct.");
+  console.log("You will have only one chance to guess the correct answer.\n");
+  console.log("All the best!\n".toUpperCase());
+}
+
+
+const conclusion = (score, count, name) => {
+  if (score === 0) {
+  console.log("Oops!! you have scored " + score + " out of " + count);
+  console.log("Practice to get better.");
+  }
+  else if(score<count/2) {
+  console.log("Well played!! you have scored " + score + " out of " + count);
+  console.log("Keep trying, you can improve");
+  }
+  else {
+  console.log("Congratulations you have scored " + score + " out of " + count);
+  }
+}
+
+function main() {
+  disp();
+  conclusion(score, count, name);
+  const again = r.question('wanna try again? (y to continue / any other key to quit)\t');
+  if(again === 'y') {
+    main();
+  }
+  console.log("Thanks for playing " + name + ". Good Bye!!\n");
+}
+
+// user defined functions end here
+
+
 const chalk = require('chalk');
 console.log(chalk.magenta("--------------------------------"));
 console.log(chalk.blue.bold("|Welcome to the Easy Quiz on JS|"));
@@ -42,73 +116,3 @@ const count = questions.length; // total number of questions
 let score = 0; // for showing the user score
 main();
 
-
-// user defined functions start from here
-function main() {
-  display(count);
-  conclusion(score, count, name);
-  const again = r.question('wanna try again? (y/any other key to quit)\t');
-  if(again === 'y') {
-    main();
-  }
-}
-
-function display() {
-  console.log('--------------------------------------------------------------------------------');
-  for(let i = 0; i < count; i++) {
-    console.log();
-    console.log("Q" + (i+1).toString() + ". " + questions[i]['question'] + '\n');
-    let optNum = questions[i]['options'].length;
-    for(const opts of questions[i]['options']) {
-      console.log('(' + (questions[i]['options'].length - (optNum--) + 1).toString() + ') ' + opts + '\n');
-    }
-    const choice = r.question("enter your choice : ");
-    validate(i , choice);
-    console.log();
-    console.log('--------------------------------------------------------------------------------');
-  }
-   
-}
-
-function validate(i, ch) 
-{
-  if(![1,2,3,4].includes(Number(ch))) {
-    console.log("You entered an invalid choice");
-  }
-
-  else if(questions[i]['answer'].toString() === ch) {
-    console.log("Well done!! you have answered correctly.");
-    ++score;
-  }
-
-  else {
-    console.log("Oopsie!! That is incorrect");
-    console.log("The correct answer is : " + questions[i]['answer'].toString());
-  }
-
-  console.log();
-}
-
-function instructions(name) {
-  console.log("\nGreetings " + name.toUpperCase()+"!");
-  console.log(chalk.redBright.underline.bold("PLEASE FOLLOW THE INSTRUCTIONS \n"));
-  console.log("Each question will have four choices of which only one is correct.");
-  console.log("You will have only one chance to guess the correct answer.\n");
-  console.log("All the best!\n".toUpperCase());
-}
-
-
-function conclusion(score, count, name) {
-  if (score === 0) {
-  console.log("Oops!! you have scored " + score + " out of " + count);
-  console.log("Practice to get better.");
-  }
-  else if(score<count/2) {
-  console.log("Well played!! you have scored " + score + " out of " + count);
-  console.log("Keep trying, you can improve");
-  }
-  else {
-  console.log("Congratulations you have scored " + score + " out of " + count);
-  }
-  console.log("Thanks for playing " + name + ". Good Bye!!\n");
-}
